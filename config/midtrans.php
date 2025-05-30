081a67873dd4817e0fc4d0d3f193ae959c5e4070
Alaaddin74 <48843837+Alaaddin74@users.noreply.github.com>
2025-05-31 00:06:17 +0700

    midrans

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,1 +1,21 @@
-return 
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Midtrans Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for Midtrans payment gateway.
+    | You can set your server key, client key, and other configurations here.
+    |
+    */
+
+    'server_key' => env('MIDTRANS_SERVER_KEY', 'your-server-key-here'),
+    // 'client_key' => env('MIDTRANS_CLIENT_KEY', 'your-client-key-here'),
+    'is_production' => env('MIDTRANS_IS_PRODUCTION'),
+    'is_sanitized' => env('MIDTRANS_IS_SANITIZED'),
+    'is_3ds' => env('MIDTRANS_IS_3DS'),
+    // Additional configurations can be added here
+];
+// You can also add more configurations like payment methods, notification URLs, etc.
